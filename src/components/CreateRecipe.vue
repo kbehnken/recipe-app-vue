06e1827f4cc78ecef3a9d7e437490fe86bd0fd05
Katrina Behnken <katrina.behnken@gmail.com>
2021-01-10 18:10:21 -0700

    Reorganize data and add code to save photo and

--- src/components/CreateRecipe.vue
+++ src/components/CreateRecipe.vue
@@ -6,23 +6,37 @@
                 Fill out the form below and click the Add button to add a new recipe to the recipe box.<br /><br />
             </p>
             <div class='form-container'>  
-                <VTextField outlined v-model='recipeName' name='recipeName' placeholder='Recipe Name' />
-                <VTextField outlined v-model='fromTheKitchenOf' name='fromTheKitchenOf' placeholder='From the kitchen of' />
-                <VTextField outlined v-model='description' name='description' placeholder='Description' />
-                <VTextField outlined v-model='serves' name='serves' placeholder='Serves' />
-                <VTextField outlined v-model='prepTime' name='prepTime' placeholder='Prep Time' />
-                <VTextField outlined v-model='cookTime' name='cookTime' placeholder='Cook Time' />
+                <VTextField outlined v-model='recipe.name' name='name' placeholder='Recipe Name' />
+                <VTextField outlined v-model='recipe.fromTheKitchenOf' name='fromTheKitchenOf' placeholder='From the kitchen of' />
+                <VTextField outlined v-model='recipe.description' name='description' placeholder='Description' />
+                <VTextField outlined v-model='recipe.serves' name='serves' placeholder='Serves' />
+                <VTextField outlined v-model='recipe.prepTime' name='prepTime' placeholder='Prep Time' />
+                <VTextField outlined v-model='recipe.cookTime' name='cookTime' placeholder='Cook Time' />
                 <VTextField outlined v-model='quantity' name='quantity' placeholder='Quantity' />
                 <VTextField outlined v-model='ingredient' name='ingredient' placeholder='Ingredient' />
                 <button v-on:click='handleAddIngredient'>
                     +
                 </button>
-                <draggable v-model='ingredients' name='draggableIngredients'>
-                    <div v-for='ingredient in ingredients' :key='ingredient.name' style='background-color: #eee; padding: 10px;'>
-                        {{ingredient.quantity}} {{ingredient.name}} <span v-on:click='handleDeleteIngredient'>-</span>
+                <draggable v-model='recipe.ingredients' name='draggableIngredients'>
+                    <div v-for='(ingredient, index) in recipe.ingredients' :key='ingredient.name' style='background-color: #eee; padding: 10px;'>
+                        {{ingredient.quantity}} {{ingredient.name}} <span v-on:click='handleDeleteIngredient(index)'>-</span>
                     </div>
                 </draggable>
-                <VTextField outlined v-model='directions' name='directions' placeholder='Directions' />
+                <VTextField outlined v-model='recipe.directions' name='directions' placeholder='Directions' />
+                <div>
+                    <label>Upload Photo:</label><br />
+                    <span className='fine-print'>
+                        (.jpg, .gif, .png)<br />
+                    </span>
+                </div>
+                <div>
+                    <VFileInput v-model="photo" name='photo' /><br /><br />
+                    <br /><br />
+                </div>
+                <div>
+                    <VCheckbox v-model="recipe.isShared" />
+                    <label>Publish?</label>
+                </div>
                 <button v-on:click='handleAddRecipe()'>
                     Save
                 </button>
@@ -45,44 +59,43 @@ export default {
     },
     data: function () {
         return {
-            recipeName: '',
-            fromTheKitchenOf: '',
-            description: '',
-            serves: '',
-            prepTime: '',
-            cookTime: '',
-            ingredients: [],
+            recipe: { 
+                name: '',
+                fromTheKitchenOf: '',
+                description: '',
+                serves: '',
+                prepTime: '',
+                cookTime: '',
+                ingredients: [],
+                directions: '',
+                isShared: false
+            },
+            recipeId: '',
             quantity: '',
             ingredient: '',
-            directions: '',
-            isShared: false
+            photo: []
         }
     },
     methods: {
         handleAddIngredient() {
-            this.ingredients.push({
+            this.recipe.ingredients.push({
                 quantity: this.quantity,
                 name: this.ingredient
             })
             this.quantity = ''
             this.ingredient = ''
         },
-        handleDeleteIngredient() {
-            this.ingredients.splice(this.ingredient, 1)
+        handleDeleteIngredient(index) {
+            this.recipe.ingredients.splice(index, 1)
         },
         handleAddRecipe() {
-            axios.post('http://localhost:3030/api/v1/recipes', {
-                name: this.recipeName,
-                fromTheKitchenOf: this.fromTheKitchenOf,
-                description: this.description,
-                serves: this.serves,
-                prepTime: this.prepTime,
-                cookTime: this.cookTime,
-                ingredients: this.ingredients,
-                directions: this.directions,
-                isShared: this.isShared
-            },
-            {
+            let formData = new FormData();
+            console.log(this.recipe)
+
+            formData.append('recipe', JSON.stringify(this.recipe))
+            formData.append('photo', this.photo)
+
+            axios.post(`${process.env.VUE_APP_API_PROTOCOL}${process.env.VUE_APP_API_SERVER}:${process.env.VUE_APP_API_PORT}/api/v1/recipes`, formData, {
                 headers: authHeader()
             }
             )
