cf1304bab5014bec1601c087cc34b74b9f459ed4
Katrina Behnken <katrina.behnken@gmail.com>
2021-01-10 17:56:13 -0700

    Add code to load photo and truncate text

--- src/components/RecipeTile.vue
+++ src/components/RecipeTile.vue
@@ -1,29 +1,64 @@
 <template>
-    <router-link :to="{path: '/recipe-card/' + recipe._id}">
+    <router-link :to="{path: '/recipe-card/' + recipe._id}" style="color: #424242;">
         <div class='recipe-tile'>
             <div>
                 <router-link :to="{path: '/recipe-card/' + recipe._id}">
+                    <img v-bind:src="src" style="height: 225px; maxWidth: 100%;" />
                     <h1>
-                        {{recipe.name}}
+                        {{truncatedRecipeName}}
                     </h1>
                 </router-link>
-                {{recipe.description}}
+                {{truncatedDescription}}
             </div>
         </div>
     </router-link>
 </template>
 
 <script>
+import { authHeader } from '../helpers/authHeader';
+
 export default {
     name: 'RecipeTile',
     props: {recipe: Object},
-    methods: {
-        truncateRecipeName(recipeName) {
-            if (recipeName.length <= 20) {
-                return recipeName;
+    data: function () {
+        return {
+            src: ''
+        }
+    },
+    mounted() {
+        const url=`${process.env.VUE_APP_API_PROTOCOL}${process.env.VUE_APP_API_SERVER}:${process.env.VUE_APP_API_PORT}/api/v1/recipes/photos/${this.recipe._id}`;
+        
+        fetch(url, {headers: authHeader()})
+        .then(async res => {
+            if (res.status === 200) {
+                this.src = URL.createObjectURL(await res.blob());
+                console.log(this.src)
+            } else {
+                console.log(res.status)
             }
-            return recipeName.slice(0, 20) + '...';
+        })
+        .catch(err => {
+            console.log(err);
+        });  
+    },
+    computed: {
+        truncatedRecipeName() {
+            const { name } = this.recipe
+            if (name.length <= 20) {
+                return name;
+            }
+            return name.slice(0, 20) + '...';
         },
+        truncatedDescription() {
+            const { description } = this.recipe
+            if (description.length <= 20) {
+                return description;
+            }
+            return description.slice(0, 90) + '...';
+        },
+        imgSrc() {
+            return this.src;
+        }
     }
 }
 </script>
