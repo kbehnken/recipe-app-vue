a81181c03372ff297d62d843b718651633d7325a
Katrina Behnken <katrina.behnken@gmail.com>
2021-01-14 09:07:08 -0700

    Update CSS

--- src/components/CreateRecipe.vue
+++ src/components/CreateRecipe.vue
@@ -1,25 +1,25 @@
 <template>
     <div>
         <Header />
-        <div class='outer-content-container'>
-            <p style='text-align: center;'>
+        <div class="outer-content-container">
+            <p style="text-align: center;">
                 Fill out the form below and click the Add button to add a new recipe to the recipe box.<br /><br />
             </p>
-            <div class='form-container'>  
-                <VTextField outlined v-model='recipe.name' name='name' placeholder='Recipe Name' />
-                <VTextField outlined v-model='recipe.fromTheKitchenOf' name='fromTheKitchenOf' placeholder='From the kitchen of' />
-                <VTextField outlined v-model='recipe.description' name='description' placeholder='Description' />
-                <VTextField outlined v-model='recipe.serves' name='serves' placeholder='Serves' />
-                <VTextField outlined v-model='recipe.prepTime' name='prepTime' placeholder='Prep Time' />
-                <VTextField outlined v-model='recipe.cookTime' name='cookTime' placeholder='Cook Time' />
-                <VTextField outlined v-model='quantity' name='quantity' placeholder='Quantity' />
-                <VTextField outlined v-model='ingredient' name='ingredient' placeholder='Ingredient' />
-                <button v-on:click='handleAddIngredient'>
+            <div class="form-container">  
+                <VTextField outlined v-model="recipe.name" name="name" placeholder="Recipe Name" />
+                <VTextField outlined v-model="recipe.fromTheKitchenOf" name="fromTheKitchenOf" placeholder="From the kitchen of" />
+                <VTextField outlined v-model="recipe.description" name="description" placeholder="Description" />
+                <VTextField outlined v-model="recipe.serves" name="serves" placeholder="Serves" />
+                <VTextField outlined v-model="recipe.prepTime" name="prepTime" placeholder="Prep Time" />
+                <VTextField outlined v-model="recipe.cookTime" name="cookTime" placeholder="Cook Time" />
+                <VTextField outlined v-model="quantity" name="quantity" placeholder="Quantity" />
+                <VTextField outlined v-model="ingredient" name="ingredient" placeholder="Ingredient" />
+                <button v-on:click="handleAddIngredient">
                     +
                 </button>
-                <draggable v-model='recipe.ingredients' name='draggableIngredients'>
-                    <div v-for='(ingredient, index) in recipe.ingredients' :key='ingredient.name' style='background-color: #eee; padding: 10px;'>
-                        {{ingredient.quantity}} {{ingredient.name}} <span v-on:click='handleDeleteIngredient(index)'>-</span>
+                <draggable v-model="recipe.ingredients" name="draggableIngredients">
+                    <div v-for="(ingredient, index) in recipe.ingredients" :key="ingredient.name" class="ingredient-container">
+                        {{ingredient.quantity}} {{ingredient.name}} <span v-on:click="handleDeleteIngredient(index)">-</span>
                     </div>
                 </draggable>
                 <VTextarea outlined v-model="recipe.directions" name="directions" placeholder="Directions" />
